65862df0fb106e843f00021a364b3f0bea46beb2
Ace <ace@laien.io>
2025-05-13 15:38:27 +0800

    增加测试功能

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pytz
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
@@ -13,10 +14,11 @@ scheduler = BlockingScheduler(
 )
 
 # 每小时的第 0 分钟和第 30 分钟各执行一次（即每半小时）
-scheduler.add_job(wanhao_task.wanhao_task, 'cron', minute='0,30', id='wanhao_price_job')
+scheduler.add_job(wanhao_task.wanhao_task, 'cron', minute='20,40', id='wanhao_price_job')
 scheduler.add_job(wanhao_task.get_cookie, 'cron', hour=23, minute=59, id='wanhao_cookie_job', timezone='Asia/Shanghai')
 
 # 启动调度器
 if __name__ == '__main__':
+    wanhao_task.get_cookie()
     logger.info("启动定时任务调度器...")
     scheduler.start()
